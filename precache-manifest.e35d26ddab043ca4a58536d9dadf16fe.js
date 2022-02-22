381e2cf7d4f6486cb30ff08434ce180cdbffb273
thangquan <doraemona1k44@gmail.com>
2022-02-23 00:25:27 +0700

    Updates

---
+++ precache-manifest.e35d26ddab043ca4a58536d9dadf16fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "94244c544d086040ea41",
+    "url": "/Eportfolio/static/css/main.3a7f2aea.chunk.css"
+  },
+  {
+    "revision": "94244c544d086040ea41",
+    "url": "/Eportfolio/static/js/main.94244c54.chunk.js"
+  },
+  {
+    "revision": "0e35a0bce0ec9628cd45",
+    "url": "/Eportfolio/static/js/runtime~main.0e35a0bc.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Eportfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c46d7cb07205c5b5cdcb84d5cc20e39b",
+    "url": "/Eportfolio/static/media/img1.c46d7cb0.jpg"
+  },
+  {
+    "revision": "cc8fbb34badda33625aa2bc6d205f46d",
+    "url": "/Eportfolio/static/media/hoatdong.cc8fbb34.png"
+  },
+  {
+    "revision": "dfcbd8fda1aaeebc63d6c6f38018e670",
+    "url": "/Eportfolio/static/media/hoatdong2.dfcbd8fd.png"
+  },
+  {
+    "revision": "09fd12dc0d48120ff9f2aee2b6e90348",
+    "url": "/Eportfolio/static/media/hoatdong3.09fd12dc.png"
+  },
+  {
+    "revision": "301869b5bc60d5b2f20844cda3b9fa97",
+    "url": "/Eportfolio/static/media/hoatdong4.301869b5.jpg"
+  },
+  {
+    "revision": "a899115d5fd55784c78d907c383a949b",
+    "url": "/Eportfolio/static/media/trainingjs.a899115d.png"
+  },
+  {
+    "revision": "ed806d3fdb045c91ee46525c04fa9e60",
+    "url": "/Eportfolio/static/media/imageCover.ed806d3f.jpg"
+  },
+  {
+    "revision": "71f6c4aa2c514d92435ccebe2193bd63",
+    "url": "/Eportfolio/index.html"
+  }
+];
