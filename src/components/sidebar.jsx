7b438425d36d7fbef3dd9f2fbb1debb59bde6e8f
thangquan <doraemona1k44@gmail.com>
2022-02-23 00:37:20 +0700

    a

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/avatar.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/avatar3.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Nguyễn Hữu Thắng</a></h1>
               <span className="email"><i className="icon-mail"></i> thangpaisen@gmail.com</span>
             </div>
