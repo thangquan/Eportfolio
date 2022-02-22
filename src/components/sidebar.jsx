a495a08b8fc7ad6b8b424fc3abd60b43465d834c
thangquan <doraemona1k44@gmail.com>
2022-02-23 00:35:02 +0700

    a

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -29,7 +29,7 @@ export default class Sidebar extends Component {
                 <li><a href="https://twitter.com/thngnht19" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
                 <li><a href="" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
                 <li><a href="" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/thangpaisen" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="https://github.com/thangquan" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
                 <li><a href="" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
               </ul>
             </nav>
